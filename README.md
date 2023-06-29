79aca71ca56dae039cd663b24113e2add85e5c39
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2023-06-29 19:38:31 +0700

    Create main branch

---
+++ README.md
@@ -0,0 +1,7 @@
+# Testing Refinitiv Real-Time Optimized Connection with the TestClient Tool
+- version: 1.0
+- Last update: June 2023
+- Environment: Windows/Linux
+- Prerequisite: [Prerequisite](#prerequisite)
+
+TBD
---
+++ script/getObfuscatePassword.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#obfuscated password
+result=$(./dacsObfuscatePassword -e $RAW_PASSWORD)
+#get only obfuscated password from the result
+IFS=":" read -ra ADDR <<< $result
+#print result, just for checking
+#echo "obfuscated password is ${ADDR[1]}"
+echo "obfuscated RTO password success, setting it to environment varialbe named \$PASSWORD"
+export PASSWORD=${ADDR[1]}
